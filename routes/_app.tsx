342193d5b9758a4c30a41f757601c3097ed5cd2b
inky <42044862+ghostm68@users.noreply.github.com>
2026-08-05 11:12:10 -0400

    Add files via upload

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,12 +1,11 @@
-import { define } from "../utils.ts";
+import { type PageProps } from "$fresh/server.ts";
 
-export default define.page(function App({ Component, state }) {
+export default function App({ Component }: PageProps) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{state.title ?? "with-fresh"}</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
@@ -14,4 +13,4 @@ export default define.page(function App({ Component, state }) {
       </body>
     </html>
   );
-});
+}
